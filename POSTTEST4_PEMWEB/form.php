fb5a403fd6bc4ed9037e0a9cf98227f57bc05de6
Salwa Arlinda Humairah <126219178+salwarlndh@users.noreply.github.com>
2023-10-10 19:44:29 +0700

    POSTTEST 4

---
+++ POSTTEST4_PEMWEB/form.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome ><</title>
+</head>
+<body>
+    <h1>Anda Berhasil Submit!</h1>
+    <div class="container">
+        <img src="cat.png" alt="cat">
+    </div>
+</body>
+</html>
+
+
+<style>
+    body {
+        background-color: #E5BEEC;
+
+    }
+
+    .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    }
+
+    h1 {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: #2A2F4F;
+    }
+
+    img {
+        padding: 30px 30px;
+    }
+</style>
+
